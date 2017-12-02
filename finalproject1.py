7a8dad5c99e806466fb5f2f8308497be8c9ee12c
Natalie <nmorel@wisc.edu>
2017-12-02 12:32:24 -0600

    prime

--- finalproject1.py
+++ finalproject1.py
@@ -3,12 +3,13 @@ Created on Dec 2, 2017
 
 @author: nataliemorel
 '''
-sdfsdf
-sdf
-dsf
-s
-dsfd
-sdfsdf
-sdf
-saldfjlasdf
-asfd
+from _ast import Num
+def print_factors(x):
+    list=[];
+    print("The factors of," "x," "are:")
+    for i in range (1, x+1):
+        if x%i == 0:
+            list.append(i)
+            
+num=int(input("Enter a number:"))
+print_factors(Num)
